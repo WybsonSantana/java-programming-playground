731e281c5b3b676c5f2430fa03518be9e5046543
WybsonSantana <wybson@hotmail.com>
2024-03-07 02:42:46 -0300

    jpp 02.07 - Working with variables

---
+++ 02 - Java Language Fundamentals/02.07 - Working with variables/working-with-variables/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is not recommended by Oracle's coding convention:
+        //int myAge = 120, yourAge = 45;
+
+        // lowerCamelCase
+        int myAge = 120;
+        int yourAge = 45;
+
+        int totalAge = myAge + yourAge;
+
+        System.out.println("Total ages: " + totalAge);
+    }
+}
