7b13e175ac5c3b1128cb5ba8df69633ac1aadc76
WybsonSantana <wybson@hotmail.com>
2024-03-07 02:04:41 -0300

    jpp 02.04 - Writing comments in code

---
+++ 02 - Java Language Fundamentals/02.04 - Writing comments in code/writing-comments/src/Main.java
@@ -0,0 +1,9 @@
+// You can comment an entire line
+public class Main { // Or in front of a statement and/or instruction
+    /*
+    public static void main(String[] args) {
+    // Print the message to standard output
+        System.out.println("Hello, world!");
+    }
+    */
+}
