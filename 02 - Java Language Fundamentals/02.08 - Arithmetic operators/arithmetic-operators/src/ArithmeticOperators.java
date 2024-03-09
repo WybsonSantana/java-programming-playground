1608d46f119841d592ce7df05ba736f999c4574f
WybsonSantana <wybson@hotmail.com>
2024-03-09 16:22:52 -0300

    02.08 - Arithmetic operators

---
+++ 02 - Java Language Fundamentals/02.08 - Arithmetic operators/arithmetic-operators/src/ArithmeticOperators.java
@@ -0,0 +1,22 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int myAge = 120;
+        int yourAge = 130;
+
+        // Addition
+        int totalAge = myAge + yourAge + 100;
+        System.out.println("Sum of ages: " + totalAge);
+
+        // Subtraction
+        int ageDiference = yourAge - myAge;
+        System.out.println("Age difference: " + ageDiference);
+
+        // Multiplication
+        int twiceAge = yourAge * 2;
+        System.out.println("Twice age: " + twiceAge);
+
+        // Division
+        int halfAge = yourAge / 2;
+        System.out.println("Half age: " + halfAge);
+    }
+}
---
+++ 02 - Java Language Fundamentals/02.08 - Arithmetic operators/arithmetic-operators/src/AverageAgeCalculator.java
@@ -0,0 +1,11 @@
+public class AverageAgeCalculator {
+    public static void main(String[] args) {
+        int myAge = 120;
+        int yourAge = 130;
+        int otherAge = 140;
+
+        int averageAge = (myAge + yourAge + otherAge) / 3;
+
+        System.out.println("Average age: " + averageAge);
+    }
+}
