dbbf00892125638ddd79361b1281bd7c57ba357e
WybsonSantana <wybson@hotmail.com>
2024-03-09 16:36:21 -0300

    02.09 - Challenge - arithmetic variables and operators

---
+++ 02 - Java Language Fundamentals/02.09 - Challenge - arithmetic variables and operators/challenge/src/AverageTicketCalculator.java
@@ -0,0 +1,11 @@
+public class AverageTicketCalculator {
+    public static void main(String[] args) {
+        int sale1 = 20;
+        int sale2 = 30;
+        int sale3 = 100;
+
+        int averageValuePerSale = (sale1 + sale2 + sale3) / 3;
+
+        System.out.println("Average value per sale: " + averageValuePerSale);
+    }
+}
