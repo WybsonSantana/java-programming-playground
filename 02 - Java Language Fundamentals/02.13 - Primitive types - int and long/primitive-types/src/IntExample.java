8fe54506076fada2508f66a2acabc5746e3b5ee3
WybsonSantana <wybson@hotmail.com>
2024-03-10 17:50:47 -0300

    02.13 - Primitive types - int and long

---
+++ 02 - Java Language Fundamentals/02.13 - Primitive types - int and long/primitive-types/src/IntExample.java
@@ -0,0 +1,8 @@
+public class IntExample {
+    public static void main(String[] args) {
+        int number = 2_147_483_647;
+        //int identifier = 2_147_483_648; // Doesn't compile
+
+        System.out.println("Max int value: " + number);
+    }
+}
---
+++ 02 - Java Language Fundamentals/02.13 - Primitive types - int and long/primitive-types/src/LongExample.java
@@ -0,0 +1,9 @@
+public class LongExample {
+    public static void main(String[] args) {
+        long number = 2_147_483_648L;
+
+        System.out.println("number value: " + number);
+        System.out.println(2_147_483_648L);
+        //System.out.println(2_147_483_648); // Doesn't compile
+    }
+}
