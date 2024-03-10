a6aeb3e5df8fcaca2db02e42e673b18d60eb71eb
WybsonSantana <wybson@hotmail.com>
2024-03-10 20:35:07 -0300

    02.20 - Using escape sequences

---
+++ 02 - Java Language Fundamentals/02.20 - Using escape sequences/using-escape-sequences/src/EscapeSequences.java
@@ -0,0 +1,9 @@
+public class EscapeSequences {
+    public static void main(String[] args) {
+        System.out.println("Hello, \"Mary\"!");
+
+        System.out.println("Your name:\nJohn Due");
+
+        System.out.println("C:\\Windows");
+    }
+}
