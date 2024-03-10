a67e215aa81aa8f405a0450e156d1d17468a4611
WybsonSantana <wybson@hotmail.com>
2024-03-10 19:55:48 -0300

    02.18 - Challenge - arithmetic promotion

---
+++ 02 - Java Language Fundamentals/02.18 - Challenge - arithmetic promotion/challenge/src/AverageAge.java
@@ -0,0 +1,11 @@
+public class AverageAge {
+    public static void main(String[] args) {
+        int age1 = 30;
+        int age2 = 40;
+        int age3 = 60;
+
+        float averageAge = (age1 + age2 + age3) / 3F;
+
+        System.out.println("Average age: " + averageAge);
+    }
+}
